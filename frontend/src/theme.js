88ba53bcd958661ed3600a9cc74782dc09cc72cb
ferdinand7103 <ferdinand7103@gmail.com>
2026-09-18 23:13:59 +1000

    Change theme to teal for automated deployment demonstration

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#6A1B9A",
+      main: "#00695C",
     },
     secondary: {
       main: "#2E7D32",
